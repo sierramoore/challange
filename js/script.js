1f71c00f455ed847996eab4a3c71330159282a63
Sierra Moore <sierra.v.moore@gmail.com>
2020-02-02 23:47:32 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+(function () {
+    const popup = document.getElementById('popup');
+
+    const btn = document.getElementById('btn-modal');
+    btn.onclick = (e) => {
+        e.preventDefault();
+        popup.style.display = "block";
+        popup.style.visibility = "visible";
+    };
+
+// close modal with escape btn
+    document.addEventListener("keydown", (e) => {
+        if (e.keycode === 27) {
+            popup.style.display = "none";
+        }
+    });
+
+// close modal on outside click, clicking escape key or x
+    window.onclick = function(e) {
+        const x = document.getElementById('popup-x');
+        if (e.target === popup || e.target === x) {
+            popup.style.display = "none";
+        }
+    };
+})(); //iief for scope
